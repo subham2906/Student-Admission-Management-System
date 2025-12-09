6bbff66a9e404c0f4ea8beb185155bf963973d70
Subham Kumar Gupta <subhamkumargupta2906@gmail.com>
2025-12-09 12:11:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import tkinter as tk
-from add_student import open_add_student_window
-from view_students import open_view_window
+from tkinter import ttk
+from student_form import AddStudentFrame
+from storage import get_all_students
 
-root = tk.Tk()
-root.title("Student Admission Management System")
-root.geometry("450x350")
+class StudentApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Student Admission Management System")
+        self.geometry("800x600")
+        self.create_tabs()
 
-tk.Label(root, text="Student Admission System", font=("Arial", 18, "bold")).pack(pady=30)
+    def create_tabs(self):
+        tabControl = ttk.Notebook(self)
+        self.tab1 = AddStudentFrame(tabControl)
+        self.tab2 = ttk.Frame(tabControl)
 
-tk.Button(root, text="Add Student", width=20, font=("Arial", 12),
-          command=open_add_student_window, bg="#007bff", fg="white").pack(pady=15)
+        tabControl.add(self.tab1, text='Add Student')
+        tabControl.add(self.tab2, text='View Students')
+        tabControl.pack(expand=1, fill="both")
 
-tk.Button(root, text="View All Students", width=20, font=("Arial", 12),
-          command=open_view_window, bg="#28a745", fg="white").pack(pady=15)
+        self.create_view_tab()
 
-tk.Button(root, text="Exit", width=20, font=("Arial", 12),
-          command=root.destroy, bg="red", fg="white").pack(pady=15)
+    def create_view_tab(self):
+        columns = ["First","Middle","Last","DOB","Gender","Email","Phone","City","State","Country","Physics","Chemistry","Maths","PCM%","Hobbies"]
+        self.tree = ttk.Treeview(self.tab2, columns=columns, show="headings")
+        for col in columns:
+            self.tree.heading(col, text=col)
+            self.tree.column(col,width=80)
+        self.tree.pack(expand=True, fill="both")
+        self.refresh_button = tk.Button(self.tab2, text="Refresh", command=self.refresh_table)
+        self.refresh_button.pack(pady=5)
 
-root.mainloop()
+    def refresh_table(self):
+        for row in self.tree.get_children():
+            self.tree.delete(row)
+        for student in get_all_students():
+            self.tree.insert("",tk.END,[student[col] for col in student.keys()])
+
+if __name__ == "__main__":
+    app = StudentApp()
+    app.mainloop()
